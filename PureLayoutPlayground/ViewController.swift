ff2a28cdcddb36cb403f3e9702dfbb6fd9878ff3
tramalho <thiagofacul@gmail.com>
2021-03-18 21:46:53 -0300

    Initial Commit

---
+++ PureLayoutPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PureLayoutPlayground
+//
+//  Created by Thiago Ramalho on 18/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
